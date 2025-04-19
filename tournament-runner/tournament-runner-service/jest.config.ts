b670c6a426dbdcd9b683ed944a10785e1ccb009c
Ric Meal <6094762+rdmeal@users.noreply.github.com>
2025-04-19 17:12:34 +0100

    chore: tournament runner app scaffold

---
+++ tournament-runner/tournament-runner-service/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'tournament-runner-service',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../coverage/tournament-runner/tournament-runner-service',
+};
